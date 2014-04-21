d7c4be05c2a73807e64de36ba711fd4d2940976d
Tom <woodwart@gmail.com>
2014-04-21 12:54:31 -0400

    make expressions impliment the text extension for their strings resolves #69

--- src/Lavender/extensions/text.php
+++ src/Lavender/extensions/text.php
@@ -3,6 +3,7 @@
 class Lavender_Extension_Text extends Lavender_Node
 {
   private $_bits = array();
+  private $_stop_characters = array("\n");
 
   public function tokenize_content(Lavender_Content $content)
   {
@@ -10,9 +11,8 @@ class Lavender_Extension_Text extends Lavender_Node
       $content->consume_next(); // the '|'
     }
 
-    # the rest of the line should just be text
-    while ($content->peek() !== "\n" && $content->peek()) {
-      $this->_bits[] = $content->consume_until("#\n");
+    while ($content->peek() && !in_array($content->peek(), $this->_stop_characters)) {
+      $this->_bits[] = $content->consume_until(implode('', array_merge($this->_stop_characters, array('#'))));
 
       if ($content->peek(2) == '#{') {
         $content->consume_next(2); // the '#{'
@@ -23,11 +23,22 @@ class Lavender_Extension_Text extends Lavender_Node
 
         if ($content->peek() == '}') {
           $content->consume_next(); // the '}'
-        } else {
+        }
+        else {
           throw new Lavender_Exception($content, 'found "#{" in string with no matching "}"');
         }
       }
+      elseif($content->peek() == '#') {
+        $this->_bits[] = $content->consume_next(); // the '#'
+      }
     }
+  }
+
+  public function add_stop($character)
+  {
+    $this->_stop_characters[] = $character;
+
+    return $this;
   }
 
   public function compile(array &$scope)
@@ -37,10 +48,14 @@ class Lavender_Extension_Text extends Lavender_Node
     foreach($this->_bits as $bit) {
       if (gettype($bit) == 'string') {
         $text .= $bit;
-      } else {
+      }
+      else {
         $text .= $bit->compile($scope);
       }
     }
+
+    $text = str_replace('\n', "\n", $text);
+    $text = str_replace('\t', "\t", $text);
 
     return $text;
   }
