0d5f998a3cfb071b27e9e942aaf8e6b88210fb5c
Tom <woodwart@gmail.com>
2015-02-03 12:02:11 -0500

    add sink and float filters

---
+++ src/Lavender/filters/float.php
@@ -0,0 +1,31 @@
+<?php
+
+class Lavender_Filter_Float
+{
+  public function execute(array $array, $items)
+  {
+    if (is_string($items)) {
+      $items = array($items);
+    }
+
+    usort($array, function($thing1, $thing2) use ($items) {
+      $thing1value = array_search($thing1, $items);
+      $thing2value = array_search($thing2, $items);
+      $thing1present = $thing1value !== FALSE;
+      $thing2present = $thing2value !== FALSE;
+
+      if ($thing1present && (!$thing2present || $thing1value < $thing2value)) {
+        return -1;
+      }
+      if ($thing2present && (!$thing1present || $thing2value < $thing1value)) {
+        return 1;
+      }
+
+      return 0;
+    });
+
+    return $array;
+  }
+}
+
+Lavender::register_filter('float', 'Lavender_Filter_Float');
