6f192db5a4dc70bf4bfe3a89b15a53910b670c27
Tom <woodwart@gmail.com>
2015-02-11 14:19:14 -0500

    update float/sink/sort logic to work with arrays/objects

--- src/Lavender/filters/float.php
+++ src/Lavender/filters/float.php
@@ -2,15 +2,27 @@
 
 class Lavender_Filter_Float
 {
-  public function execute(array $array, $items)
+  public function execute(array $array, $items = array(), $property = NULL)
   {
     if (is_string($items)) {
       $items = array($items);
     }
 
-    usort($array, function($thing1, $thing2) use ($items, $array) {
-      $thing1_float_value = array_search($thing1, $items);
-      $thing2_float_value = array_search($thing2, $items);
+    usort($array, function($thing1, $thing2) use ($items, $array, $property) {
+      $thing1_value = $thing1;
+      $thing2_value = $thing2;
+
+      if ($property && is_array($thing1) && is_array($thing2)) {
+        $thing1_value = $thing1[$property];
+        $thing2_value = $thing2[$property];
+      }
+      else if ($property && is_object($thing1) && is_object($thing2)) {
+        $thing1_value = $thing1->{$property};
+        $thing2_value = $thing2->{$property};
+      }
+
+      $thing1_float_value = array_search($thing1_value, $items);
+      $thing2_float_value = array_search($thing2_value, $items);
 
       $thing1_natural_value = array_search($thing1, $array);
       $thing2_natural_value = array_search($thing2, $array);
--- src/Lavender/filters/sort.php
+++ src/Lavender/filters/sort.php
@@ -2,9 +2,26 @@
 
 class Lavender_Filter_Sort
 {
-  public function execute(array $array)
+  public function execute(array $array, $property = NULL)
   {
-    asort($array);
+    if ($property) {
+      usort($array, function ($thing1, $thing2) use ($property) {
+        if (is_array($thing1) && is_array($thing2)) {
+          $value1 = $thing1[$property];
+          $value2 = $thing2[$property];
+        }
+        else {
+          $value1 = $thing1->{$property};
+          $value2 = $thing2->{$property};
+        }
+
+        return strcmp($value1, $value2);
+      });
+    }
+    else {
+      asort($array);
+    }
+
     return $array;
   }
 }
