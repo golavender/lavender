eac7058c072fc41867dba74f43959ef7852c5d6b
Dan Mowchan <dan@mowchan.com>
2015-12-14 14:33:04 -0500

    Filter for url encoding

---
+++ src/Lavender/filters/url_encode.php
@@ -0,0 +1,11 @@
+<?php
+
+class Lavender_Filter_Url_Encode
+{
+  public function execute($string)
+  {
+    return urlencode($string);
+  }
+}
+
+Lavender::register_filter('url_encode', 'Lavender_Filter_Json');
